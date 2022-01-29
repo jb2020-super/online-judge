ce39126a18596978607424ac88467cacab07d890
jb2020-super <jabirshi2020@gmail.com>
2022-01-29 12:46:20 +0800

    1996. 游戏中弱角色的数量.cpp，题解

--- leetcode/题库/1996. 游戏中弱角色的数量.cpp
+++ leetcode/题库/1996. 游戏中弱角色的数量.cpp
@@ -1,3 +1,7 @@
+/*
+Accepted but very bad running time.
+但是如果本题的数值范围是小数或者范围很大时，这种方法也可一用。
+*/
 #include <vector>
 #include <algorithm>
 
@@ -13,24 +17,49 @@ public:
 			return first[1] < second[1];
 		});
 		int len = properties.size();
-		vector<int> range_max(len, 0);
-		vector<int> range_max2(len, 0);
-		int max_val = properties[len - 1][1];
-		int max_val2 = properties[len - 1][0];
-		for (int i = len - 1; i >= 0; --i) {
-			if (properties[i][1] > max_val) {
-				max_val = properties[i][1];
-			}
-			range_max[i] = max_val;
-			if (properties[i][0] > max_val2) {
-				max_val2 = properties[i][0];
-			}
-			range_max2[i] = max_val2;
-		}
-		int count = 0;
 		for (int i = 0; i < len; ++i) {
-			if (properties[i][1] < range_max[i] && properties[i][0] < range_max2[i]) {
-				++count;
+			properties[i].push_back(i);
+		}
+		sort(properties.begin(), properties.end(), [](vector<int>& first, vector<int>& second) {
+			if (first[1] != second[1]) {
+				return first[1] < second[1];
+			}
+			return first[0] < second[0];
+		});
+		for (int i = 0; i < len; ++i) {
+			properties[i][2] += i;
+		}
+		sort(properties.begin(), properties.end(), [](vector<int>& first, vector<int>& second) {
+			if (first[2] != second[2]) {
+				return first[2] < second[2];
+			}
+			else if (first[0] != second[0]) {
+				return first[0] < second[0];
+			}
+			return first[1] < second[1];
+		});
+
+		int count = 0;
+		int last = 0;
+		for (int i = 0; i < len; ++i) {
+			if (last > 0) {
+				if (properties[last][0] > properties[i][0] &&
+					properties[last][1] > properties[i][1]) {
+					++count;
+					continue;
+				}
+			}
+			for (int j = len - 1; j > i; --j) {
+				if ((properties[j][2] < properties[i][2] + 2) /*||
+					(properties[j][0] + properties[j][1]) < (properties[i][0] + properties[i][1] + 2)*/) {
+					break;
+				}
+				if (properties[j][0] > properties[i][0] &&
+					properties[j][1] > properties[i][1]) {
+					++count;
+					last = j;
+					break;
+				}
 			}
 		}
 		return count;
@@ -46,4 +75,34 @@ public:
 [[3,3],[2,2],[1,1]]
 [[3,3],[2,2],[1,1],[4,4]]
 [[1,1],[2,1],[2,2],[1,2]]
+[[7,9],[10,7],[6,9],[10,4],[7,5],[7,10]]
 */
+/*
+速度最快的算法解读：
+1.利于了数值范围最大是10^5的条件；
+2.数值的第一维是攻击力，第二维是防御力；
+3.数组b的含义，b[k]代表攻击力为k的角色中，防御力最大的那个值；
+4.mn记录了最大的攻击力数值；
+5.数组r的含义，r[j]代表比攻击力j大的那些角色中，防御力最大的数值；
+6.b和r数组预处理好之后，对于给定的一个角色数值[x,y]，查询r[x]得到比攻击力x大的角色中防御力的最大数值；
+如果r[x] > y，则说明此角色是弱角色。
+*/
+class SolutionFastest {
+public:
+	int b[100005], r[100005];
+	int numberOfWeakCharacters(vector<vector<int>>& a) {
+		int n = a.size(), cnt = 0, mx = 0, mn = 0;
+		for (int i = 0; i < n; ++i) {
+			b[a[i][0]] = max(b[a[i][0]], a[i][1]);
+			mn = max(mn, a[i][0]);
+		}
+		for (int i = mn; ~i; --i) {
+			r[i] = max(r[i + 1], mx);
+			mx = max(mx, b[i]);
+		}
+		for (int i = 0; i < n; ++i) {
+			if (r[a[i][0]] > a[i][1]) cnt++;
+		}
+		return cnt;
+	}
+};
